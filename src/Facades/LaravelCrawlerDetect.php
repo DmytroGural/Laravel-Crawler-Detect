d599c2eb65193fab8b2ff99884cefce426560732
Mark Beech <mbeech@mark-beech.co.uk>
2026-07-11 09:16:49 +0100

    Bind CrawlerDetect for dependency injection and add facade docblocks

--- src/Facades/LaravelCrawlerDetect.php
+++ src/Facades/LaravelCrawlerDetect.php
@@ -3,7 +3,18 @@
 namespace Jaybizzle\LaravelCrawlerDetect\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jaybizzle\CrawlerDetect\CrawlerDetect;
 
+/**
+ * @method static bool isCrawler(string|null $userAgent = null)
+ * @method static string|null getMatches()
+ * @method static string|null getUserAgent()
+ * @method static string|null setUserAgent(string|null $userAgent = null)
+ * @method static void setHttpHeaders(array|null $httpHeaders = null)
+ * @method static array getUaHttpHeaders()
+ *
+ * @see CrawlerDetect
+ */
 class LaravelCrawlerDetect extends Facade
 {
     /**
@@ -13,6 +24,6 @@ class LaravelCrawlerDetect extends Facade
      */
     protected static function getFacadeAccessor()
     {
-        return 'LaravelCrawlerDetect';
+        return CrawlerDetect::class;
     }
 }
